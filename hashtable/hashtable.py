03abe9cf7b166c19f1d7fc785486f29221dd1d18
Akhil Raju <akhildraju@gmail.com>
2020-08-31 16:00:21 -0700

    day 1

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -22,6 +22,9 @@ class HashTable:
 
     def __init__(self, capacity):
         # Your code here
+        self.capacity  = capacity
+        self.table = [None] * capacity
+
 
 
     def get_num_slots(self):
@@ -35,6 +38,7 @@ class HashTable:
         Implement this.
         """
         # Your code here
+        pass
 
 
     def get_load_factor(self):
@@ -44,6 +48,7 @@ class HashTable:
         Implement this.
         """
         # Your code here
+        pass
 
 
     def fnv1(self, key):
@@ -63,6 +68,11 @@ class HashTable:
         Implement this, and/or FNV-1.
         """
         # Your code here
+        hash = 5381
+        for  c in key:
+            hash = ((hash << 5) + hash)  + ord(c)
+        
+        return hash &  0xFFFFFFFF
 
 
     def hash_index(self, key):
@@ -82,6 +92,11 @@ class HashTable:
         Implement this.
         """
         # Your code here
+        index = self.hash_index(key)
+
+        # Store the value at that slot
+        self.table[index] = value
+
 
 
     def delete(self, key):
@@ -93,6 +108,10 @@ class HashTable:
         Implement this.
         """
         # Your code here
+        index = self.hash_index(key)
+
+        self.table[index] = None
+
 
 
     def get(self, key):
@@ -104,6 +123,10 @@ class HashTable:
         Implement this.
         """
         # Your code here
+        index = self.hash_index(key)
+
+        return self.table[index]
+
 
 
     def resize(self, new_capacity):
@@ -114,7 +137,7 @@ class HashTable:
         Implement this.
         """
         # Your code here
-
+        pass
 
 
 if __name__ == "__main__":
